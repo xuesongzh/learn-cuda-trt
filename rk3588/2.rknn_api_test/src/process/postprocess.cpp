b5abb0363438b7440acc91a66eeb22bc067b7332
zxsong <zxsong13@gmail.com>
2023-12-12 21:30:00 +0800

    add rk3588

---
+++ rk3588/2.rknn_api_test/src/process/postprocess.cpp
@@ -0,0 +1,37 @@
+
+#include "postprocess.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <algorithm>
+
+#include "utils/logging.h"
+
+int get_top(float* pfProb, float* pfMaxProb, uint32_t* pMaxClass, uint32_t outputCount,
+            uint32_t topNum) {
+    uint32_t i, j;
+
+#define MAX_TOP_NUM 20
+    if (topNum > MAX_TOP_NUM)
+        return 0;
+
+    memset(pfMaxProb, 0, sizeof(float) * topNum);
+    memset(pMaxClass, 0xff, sizeof(float) * topNum);
+
+    for (j = 0; j < topNum; j++) {
+        for (i = 0; i < outputCount; i++) {
+            if ((i == *(pMaxClass + 0)) || (i == *(pMaxClass + 1)) || (i == *(pMaxClass + 2)) ||
+                (i == *(pMaxClass + 3)) || (i == *(pMaxClass + 4))) {
+                continue;
+            }
+
+            if (pfProb[i] > *(pfMaxProb + j)) {
+                *(pfMaxProb + j) = pfProb[i];
+                *(pMaxClass + j) = i;
+            }
+        }
+    }
+
+    return 1;
+}
